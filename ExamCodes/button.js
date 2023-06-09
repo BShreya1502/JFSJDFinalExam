05281418252238c2c2c10e9e043e7e427f7ed67e
ShreyaBahadule <shreyabahadule15@gmail.com>
2023-06-09 19:58:56 +0530

    Final exam submission

---
+++ ExamCodes/button.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  // change background color
+  document.body.style.backgroundColor = 'blue';
+});
